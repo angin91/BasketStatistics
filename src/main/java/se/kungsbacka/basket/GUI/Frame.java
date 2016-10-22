5c6fcaf4c419523acadc05c6cde34b3e8a971d33
angin91 <angin.andreas@gmail.com>
2016-10-22 16:43:26 +0200

    Added file browser
    Added so when you click on the new game button you'll open a browser so
    that you can import a file.

--- src/main/java/se/kungsbacka/basket/GUI/Frame.java
+++ src/main/java/se/kungsbacka/basket/GUI/Frame.java
@@ -1,9 +1,13 @@
 package se.kungsbacka.basket.GUI;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
@@ -14,36 +18,33 @@ import se.kungsbacka.basket.entities.Game;
 import se.kungsbacka.basket.entities.Player;
 import se.kungsbacka.basket.imports.ReadExcel;
 
-public class Frame extends JFrame{
+public class Frame extends JFrame {
 
 	private JButton newGameButton;
 	private JButton seeStatButton;
-	private JList gameList;
-	private JList playerList;
-	private ArrayList<Game> games;
-	private ArrayList<Player> players;
-	//will remove
-	private ArrayList<Player> players2;
-	//Will remove
-	
+	private JList<Game> gameList;
+	private JList<Player> playerList;
+	private DefaultListModel<Game> games;
+	private DefaultListModel<Player> players;
+
 	public Frame(){
 		super("Basket statistics");
 		setLayout(new FlowLayout());
-		games = new ArrayList<Game>();
-		players = new ArrayList<Player>();
+		games = new DefaultListModel<Game>();
+		players = new DefaultListModel<Player>();
 		
 		//-----------------------Test array. Will remove----------------------------------
-		ReadExcel readExcel = new ReadExcel();
+		final ReadExcel readExcel = new ReadExcel();
 		Game game = readExcel.readExcel("files/test.xlsx");
-		games.add(game);
+		games.addElement(game);
 		game = readExcel.readExcel("files/test2.xlsx");
-		games.add(game);
+		games.addElement(game);
 		//-----------------------Test array. Will remove----------------------------------
 		
-		gameList = new JList(games.toArray());
+		gameList = new JList<Game>(games);
 		add(new JScrollPane(gameList));
 		
-		playerList = new JList();
+		playerList = new JList<Player>(players);
 		add(new JScrollPane(playerList));
 		
 		newGameButton = new JButton("New game");
@@ -56,8 +57,20 @@ public class Frame extends JFrame{
 		
 		gameList.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
+				players.clear();
 				Game game = (Game) gameList.getSelectedValue();
-				playerList.setListData(game.getPlayers().toArray());
+				for(Player player : game.getPlayers()){
+					players.addElement(player);
+				}
+			}
+		});
+		newGameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				final JFileChooser fc = new JFileChooser();
+				fc.showOpenDialog(Frame.this);
+				String s = fc.getSelectedFile().getPath();
+				Game game = readExcel.readExcel(s);
+				games.addElement(game);
 			}
 		});
 	}
