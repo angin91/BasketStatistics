3c6feb6d062828755f53a4b6d7f40f10202b13b9
angin91 <angin.andreas@gmail.com>
2016-10-22 17:08:22 +0200

    Layout change
    Changed the layout for the gui

--- src/main/java/se/kungsbacka/basket/GUI/Panel.java
+++ src/main/java/se/kungsbacka/basket/GUI/Panel.java
@@ -1,15 +1,14 @@
 package se.kungsbacka.basket.GUI;
 
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -18,7 +17,7 @@ import se.kungsbacka.basket.entities.Game;
 import se.kungsbacka.basket.entities.Player;
 import se.kungsbacka.basket.imports.ReadExcel;
 
-public class Frame extends JFrame {
+public class Panel extends JPanel {
 
 	private JButton newGameButton;
 	private JButton seeStatButton;
@@ -27,39 +26,45 @@ public class Frame extends JFrame {
 	private DefaultListModel<Game> games;
 	private DefaultListModel<Player> players;
 
-	public Frame(){
-		super("Basket statistics");
-		setLayout(new FlowLayout());
+	public Panel() {
 		games = new DefaultListModel<Game>();
 		players = new DefaultListModel<Player>();
 		
-		//-----------------------Test array. Will remove----------------------------------
+		setLayout(null);
+
+		// -----------------------Test array. Will
+		// remove----------------------------------
 		final ReadExcel readExcel = new ReadExcel();
 		Game game = readExcel.readExcel("files/test.xlsx");
 		games.addElement(game);
 		game = readExcel.readExcel("files/test2.xlsx");
 		games.addElement(game);
-		//-----------------------Test array. Will remove----------------------------------
-		
+		// -----------------------Test array. Will
+		// remove----------------------------------
+
 		gameList = new JList<Game>(games);
-		add(new JScrollPane(gameList));
-		
+		JScrollPane gameListScrollPane = new JScrollPane(gameList);
+		gameListScrollPane.setBounds(20, 20, 300, 300);
+		add(gameListScrollPane);
+
 		playerList = new JList<Player>(players);
-		add(new JScrollPane(playerList));
-		
+		JScrollPane playerListScrollPane = new JScrollPane(playerList);
+		playerListScrollPane.setBounds(340, 20, 300, 300);
+		add(playerListScrollPane);
+
 		newGameButton = new JButton("New game");
-		newGameButton.setBounds(100, 100, 50, 50);
+		newGameButton.setBounds(660, 20, 140, 140);
 		add(newGameButton);
-		
+
 		seeStatButton = new JButton("See stats");
-		seeStatButton.setBounds(100, 100, 50, 50);
+		seeStatButton.setBounds(660, 180, 140, 140);
 		add(seeStatButton);
-		
+
 		gameList.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
 				players.clear();
 				Game game = (Game) gameList.getSelectedValue();
-				for(Player player : game.getPlayers()){
+				for (Player player : game.getPlayers()) {
 					players.addElement(player);
 				}
 			}
@@ -67,11 +72,20 @@ public class Frame extends JFrame {
 		newGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				final JFileChooser fc = new JFileChooser();
-				fc.showOpenDialog(Frame.this);
+				fc.showOpenDialog(Panel.this);
 				String s = fc.getSelectedFile().getPath();
 				Game game = readExcel.readExcel(s);
 				games.addElement(game);
 			}
 		});
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Basketball statistics");
+		frame.getContentPane().add(new Panel());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 800);
+		frame.setVisible(true);
 	}
 }
